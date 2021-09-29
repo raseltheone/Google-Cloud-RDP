52f2cc6b738e26be093f8b6717a95228fc4a4bd1
raseltheone <89748816+raseltheone@users.noreply.github.com>
2021-09-29 22:21:45 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing RDP Be Patience..." >&2
 {
-sudo useradd -m NAHID
-sudo adduser NAHID sudo
-echo 'NAHID:1234' | sudo chpasswd
+sudo useradd -m RASEL
+sudo adduser RASEL sudo
+echo 'RASEL:45211' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -23,12 +23,12 @@ sudo apt -y install obs-studio
 sudo apt -y install firefox
 sudo apt -y install qbittorrent
 sudo apt install nload
-sudo adduser NAHID chrome-remote-desktop
+sudo adduser RASEL chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - NAHID -c """$CRP"""
+su - RASEL -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n'
-printf 'Your SUDO Pasword is: 1234 \n'
+printf 'Your SUDO Pasword is: 45211 \n'
